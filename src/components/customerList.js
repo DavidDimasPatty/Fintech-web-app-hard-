51734158b057e5513a4c2cbf412849a97a61f921
DavidDimasPatty <daviddimas80@gmail.com>
2022-04-07 00:53:52 +0700

    fix display

--- src/components/customerList.js
+++ src/components/customerList.js
@@ -51,7 +51,7 @@ const CustomerList = () => {
           <div className="customerCard m-3 pb-2">
             <div className="cardImage" style={{ backgroundImage: 'url(' + customers.profile_picture + ')' }} id={(customers.profile_picture === "" || customers.profile_picture == null) ? "" : "photo"}><img src={(customers.profile_picture === "" || customers.profile_picture == null) ? "https://freepikpsd.com/file/2019/10/default-profile-image-png-1-Transparent-Images.png" : ""}/></div>
             <div className="cardInfo">
-              <div className="cardLabel">{customers.name}</div>
+              <div className="cardLabel">{customers.name.replace("_"," ")}</div>
               <div className="cardLabel">{customers.email}</div>
               <div className="cardLabel">Status:</div>
               <div className={(customers.status === "Rejected") ? "fail cardLabel" : "success cardLabel"} id={(customers.status === "Pending") ? "pending" : ""}>{customers.status}</div>
